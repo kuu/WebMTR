393b1240aaebf7dceef47ae15e2a9ac589dc11dc
Kuu MIYAZAKI <kuu.miyazaki@gree.net>
2014-07-02 21:42:14 +0900

    Improved UI.

--- player.js
+++ player.js
@@ -32,10 +32,13 @@
     return new Promise(function (resolve, reject) {
       var audio = new Audio();
       audio.src = '/audio/' + item;
-      audio.addEventListener('canplay', function () {
+      audio.addEventListener('canplay', function onCanPlay() {
+        audio.removeEventListener('canplay', onCanPlay, false);
+        audio.volume = 0.5;
         resolve(audio);
       }, false);
-      audio.addEventListener('error', function () {
+      audio.addEventListener('error', function onError() {
+        audio.removeEventListener('error', onError, false);
         reject(new Error('Something wrong happened in fetching audio data.'));
       }, false);
     }).then(function (elem) {
@@ -46,13 +49,40 @@
   };
 
   Player.prototype.play = function (list) {
-    var tElem;
-    for (var i = 0, il = list.length; i < il; i++) {
-      tElem = this.elements[list[i]];
-      if (tElem) {
-        tElem.play();
+    var tSelf = this;
+
+    return new Promise(function (resolve, reject) {
+      var tElem, promiseList = [];
+
+      if (!list) {
+        reject(new Error('invalid args'));
+        return;
       }
-    }
+
+      for (var i = 0, il = list.length; i < il; i++) {
+        tElem = tSelf.elements[list[i]];
+        if (tElem) {
+          promiseList.push(tSelf.playItem(tElem));
+        }
+      }
+      resolve(Promise.all(promiseList));
+    });
+  };
+
+  Player.prototype.playItem = function (item) {
+    var tSelf = this;
+
+    return new Promise(function (resolve, reject) {
+      item.play();
+      item.addEventListener('ended', function onEnded() {
+        item.removeEventListener('ended', onEnded, false);
+        resolve();
+      }, false);
+      item.addEventListener('error', function onError() {
+        item.removeEventListener('error', onError, false);
+        reject(new Error('Something wrong happened in playback.'));
+      }, false);
+    });
   };
 
   Player.prototype.stop = function () {
@@ -68,6 +98,16 @@
 
   Player.prototype.clear = function () {
     this.elements = {};
+  };
+
+  Player.prototype.setVolume = function (value) {
+    var tElem;
+    for (var k in this.elements) {
+      tElem = this.elements[k];
+      if (tElem) {
+        tElem.volume = value;
+      }
+    }
   };
 
   window.Player = Player;
